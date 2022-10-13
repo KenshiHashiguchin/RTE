520f9f071fee5d025a0931daa3e957855c969dd8
SSyamashita <yamashita@smartsocial.co.jp>
2022-10-14 00:43:50 +0900

    置換もれ

--- web/frontend/_sample.intellij-config.js
+++ web/frontend/_sample.intellij-config.js
@@ -2,12 +2,6 @@
 // r.f. https://youtrack.jetbrains.com/issue/WEB-22717#focus=streamItem-27-1558931-0-0
 System.config({
   paths: {
-    // yyの場合
-    // '@/*': './client/yy/*',
-    // '~/*': './client/yy*',
-    // golfの場合
-    // '@/*': './client/golf/*',
-    // '~/*': './client/golf/*',
     '@@/*': './*',
     '~~/*': './*',
   },
