c8b37969123f5bb1156601184362f26c7d466d7c
KenshiHashiguchin <kenshi.hashiguchi@gmail.com>
2022-11-04 00:19:55 +0900

    add table config

--- web/api/infrastructure/dao/init.go
+++ web/api/infrastructure/dao/init.go
@@ -10,12 +10,12 @@ import "github.com/go-gorp/gorp"
 
 func initDb() *gorp.DbMap {
 	db, err := sql.Open("mysql", util.Env("MYSQL_USER")+":"+util.Env("MYSQL_PASSWORD")+"@tcp("+util.Env("MYSQL_HOST")+":"+util.Env("MYSQL_PORT")+")/rte_db?parseTime=true")
-	//db, err := sql.Open("mysql", "appuser:hogehogekenshin@tcp(mysql:3306)/rte_db?parseTime=true")
-	//log.Print(util.Env("MYSQL_USER")+":"+util.Env("MYSQL_PASSWORD")+"@tcp("+util.Env("MYSQL_HOST")+":"+util.Env("MYSQL_PORT")+")/rte_db?parseTime=true")
-	//log.Print("appuser:hogehogekenshin@tcp(mysql:3306)/rte_db?parseTime=true")
+	dbmap := gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{}}
 	if err != nil {
 		log.Fatal(err)
 	}
+	dbmap.AddTableWithName(Merchant{}, "merchants").SetKeys(true, "Id")
+	dbmap.AddTableWithName(Reserves{}, "reserves").SetKeys(true, "Id")
 
-	return &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{}}
+	return &dbmap
 }
