035bec6181bf3a2f4050a30918f11d462ee67fd9
KenshiHashiguchin <kenshi.hashiguchi@gmail.com>
2022-11-03 20:10:02 +0900

    fix contract

--- contracts/facets/ReserveFacet.sol
+++ contracts/facets/ReserveFacet.sol
@@ -6,19 +6,26 @@ import {LibMerchant} from "../libraries/LibMerchant.sol";
 import "../libraries/UniversalERC20.sol";
 import "../extension/interfaces/ISlashSubmitTransaction.sol";
 
+interface ISwap {
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
 contract ReserveFacet {
     using UniversalERC20 for IERC20;
     using SafeMath for uint256;
+
+    event SettleReservation(address to, address token, uint amount);
 
     function getReservation(string memory _paymentId) external view returns (LibReserve.Reserve memory) {
         LibReserve.Reserve storage reserve = LibReserve.reserveStorage().reserve[_paymentId];
         require(reserve.status != LibReserve.Status.None, "Don't exist paymentId");
         return reserve;
-    }
-
-    function getMerchant(string memory _paymentId) external view returns (address) {
-        LibReserve.Reserve storage reserve = LibReserve.reserveStorage().reserve[_paymentId];
-        return reserve.merchant;
     }
 
     function reserve(string memory _paymentId, address _merchant, address _token, uint _depositAmount, uint _cancelableTime, uint _withdrawableTime) external {
@@ -35,34 +42,35 @@ contract ReserveFacet {
         reserve.status = LibReserve.Status.Reserved;
 
         if (_depositAmount > 0) {
-            IERC20(_token).universalTransferFrom(
-                msg.sender,
-                address(this),
-                _depositAmount
-            );
+            IERC20(_token).universalTransferFrom(msg.sender, address(this), _depositAmount);
         }
     }
 
     function settleReservation(
-        uint256 amountIn_,
-        uint256 requiredAmountOut_,
-        address[] memory path_,
-        address[] memory feePath_,
-        string memory paymentId_,
-        string memory optional_
-    ) external payable {
-        LibReserve.isSubscriber(paymentId_);
-        LibReserve.isStatusReserved(paymentId_);
+        uint256 _amountIn,
+        uint256 _requiredAmountOut,
+        uint256 _deadline,
+        address[] calldata _path,
+        string memory _paymentId
+    ) external {
+        LibReserve.isSubscriber(_paymentId);
+        LibReserve.isStatusReserved(_paymentId);
 
         LibReserve.ReserveStorage storage reserveStorage = LibReserve.reserveStorage();
-        LibReserve.Reserve storage reserve = reserveStorage.reserve[paymentId_];
-        require(amountIn_ >= reserve.depositAmount, "Excess payments");
+        LibReserve.Reserve storage reserve = reserveStorage.reserve[_paymentId];
+        require(_amountIn >= reserve.depositAmount, "Excess payments");
 
-        uint256 amount = amountIn_.sub(reserve.depositAmount);
-        IERC20(reserve.token).universalTransfer(address(this), amount);
+        uint256 amount = _amountIn.sub(reserve.depositAmount);
+        IERC20(reserve.token).universalTransferFrom(msg.sender, address(this), amount);
 
-        IERC20(reserve.token).approve(reserveStorage.swapSubmitAddress, amountIn_);
-        ISlashSubmitTransaction(reserveStorage.swapSubmitAddress).submitTransaction(reserve.token, amountIn_, requiredAmountOut_, path_, feePath_, paymentId_, optional_, bytes(""));
+        IERC20(reserve.token).approve(reserveStorage.swapSubmitAddress, _amountIn);
+        ISwap(reserveStorage.swapSubmitAddress).swapTokensForExactTokens(
+            _requiredAmountOut,
+            _amountIn,
+            _path,
+            reserve.merchant,
+            _deadline
+        );
 
         reserve.status = LibReserve.Status.Settled;
         reserve.additionalAmount = amount;
@@ -77,8 +85,7 @@ contract ReserveFacet {
         LibReserve.Reserve storage reserve = LibReserve.reserveStorage().reserve[_paymentId];
         reserve.status = LibReserve.Status.Canceled;
 
-        IERC20(reserve.token).universalTransferFrom(
-            address(this),
+        IERC20(reserve.token).universalTransfer(
             reserve.subscriber,
             reserve.depositAmount
         );
@@ -90,10 +97,13 @@ contract ReserveFacet {
         LibReserve.Reserve storage reserve = LibReserve.reserveStorage().reserve[_paymentId];
         reserve.status = LibReserve.Status.Canceled;
 
-        IERC20(reserve.token).universalTransferFrom(
-            address(this),
-            reserve.subscriber,
-            reserve.depositAmount
+        IERC20(reserve.token).approve(reserveStorage.swapSubmitAddress, reserve.depositAmount);
+        ISwap(reserveStorage.swapSubmitAddress).swapTokensForExactTokens(
+            _requiredAmountOut,
+            _amountIn,
+            _path,
+            reserve.merchant,
+            _deadline
         );
     }
 
